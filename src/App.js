aedb2826f10394b60c8a92b1f479064450dd2beb
Emily <nemily1710@gmail.com>
2021-11-22 16:09:32 -0500

    clean code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
-import { API_URL } from "./constants";
 import { getIcecreamAxios } from "./api/icecream";
 
 import IcecreamList from "./components/IcecreamList";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import axios from "axios";
 
 function App() {
   const [icecreams, setIcecreams] = useState([]);
-  const getIcecreamFetch = () => {
-    const endpoint = "/icecream";
-    fetch(`${API_URL}${endpoint}`)
-      .then((res) => res.json())
-      .then((data) => {
-        //
-        console.log(data, "data");
-        setIcecreams(data);
-      });
-  };
+  // const getIcecreamFetch = () => {
+  //   const endpoint = "/icecream";
+  //   fetch(`${API_URL}${endpoint}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       //
+  //       console.log(data, "data");
+  //       setIcecreams(data);
+  //     });
+  // };
 
-  useEffect(async () => {
-    //getIcecreamFetch();
-    const data = await getIcecreamAxios();
-    setIcecreams(data);
+  useEffect(() => {
+    async function axios() {
+      const data = await getIcecreamAxios();
+      setIcecreams(data);
+    }
+    axios();
   }, []);
   return (
     <Container>
